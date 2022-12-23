1825cf42edea9901754e5a2311cc7db5bac11adb
CinLo <cinlos3382@gmail.com>
2022-12-23 11:51:07 -0300

    peticiones; body y query

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MongoDByWebSockets
+# Curso de Node.js: Base de Datos con MongoDB y WebSockets
+
+- Crear un servidor HTTP usando JavaScript
+- Crear una arquitectura escalable de backend con JavaScript
+- Modelar una base de datos y utilizarla para almacenar información en tiempo real
+- Entender e implementar websockets con SocketIO
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
-app.use('/', function(req, res) {
-    res.send('Hola');
+router.get('/message', function(req, res) {
+    res.send('Lista de mensajes');
 });
+
+router.delete('/message', function(req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('mensaje '+ req.body.text + ' añadido correctamente');
+});
+
+// app.use('/', function(req, res) {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('la aplicacion esta escuchando en http//localhost:3000');
