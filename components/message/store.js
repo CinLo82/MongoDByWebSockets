14293df52c4d3459d0372b5a2d377c483e8678ac
CinLo <cinlos3382@gmail.com>
2023-01-01 09:59:54 -0300

    almacenar y leer datos MongoDB

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,29 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model')
+
+const uri = "mongodb+srv://cin:cin0002@cluster0.xrouvau.mongodb.net/?retryWrites=true&w=majority";
+
+db.Promise = global.Promise;
+
+db.connect(uri, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+    .then(() => console.log('[db] Conectada con éxito'))
+    .catch(e => console.error('[db]', e));
+   
 
 function addMessage(message) {
-    list.push(message);
+    //list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessenges() {
-    return list;
+async function getMessenges() {
+  //  return list;
+  const message = await Model.find();
+  return message;
 }
 
 module.exports = {
